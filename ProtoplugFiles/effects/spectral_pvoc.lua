b73c5555222a353506a8a97e1c4e066fbe168301
Sintel <sintel.inquiries@gmail.com>
2021-04-13 15:10:54 +0200

    Update spectral_pvoc.lua

--- ProtoplugFiles/effects/spectral_pvoc.lua
+++ ProtoplugFiles/effects/spectral_pvoc.lua
@@ -31,6 +31,7 @@ freeze = false
 normalize = 0
 warp = 0
 minval = 0
+randomstore = 0
 
 -- useful constants
 local lineMax = fftSize
@@ -56,6 +57,8 @@ local warpPolynomials = {
 
 
 
+local TWOPI = 2*math.pi
+
 local hw  = ffi.new("double[?]", fftSize) -- Hann window
 for i = 0,fftSize-1 do
 	hw[i] = (1 - math.cos(2*math.pi*i/(fftSize-1)))*rescale
@@ -65,6 +68,10 @@ local function ApplyWindow (samples)
 	for i = 0,fftSize-1 do
 		samples[i] = samples[i] * hw[i]
 	end
+end
+
+local function wrap(x)
+    return (x + math.pi)%(TWOPI) - math.pi 
 end
 
 -- channel buffers
@@ -92,6 +99,9 @@ local function ApplyFilter(self)
 	end
 	
 	local pitch_ignore = pitch == 1.0 --math.abs(pitch - 1.0) < 0.01
+	
+	--local freeze = freeze or math.random() < randomstore
+	
 	-- cartesian to polar
 	if not freeze then
         for i=0,cplxSize-1 do
@@ -99,73 +109,76 @@ local function ApplyFilter(self)
             local imag = spectrum[i][1] 
             local mag = 2*math.sqrt(real*real+imag*imag)
             local angle = math.atan2(imag, real)
+            
+            local fd = feedback
+            if randomstore then
+                if feedback < math.random() then
+                    fd = 0
+                else
+                    fd = 1
+                end
+            end
 		
-            self.mag[i] = mag*(1-feedback) + self.feedb_buf[i]*feedback
+            self.mag[i] = mag*(1-fd) + self.feedb_buf[i]*fd
 		
 
             self.feedb_buf[i] = self.mag[i]
-            self.phase_delta[i] = (angle - self.phase[i]  + math.pi)%(2*math.pi) - math.pi 
-		
-		
-            --[[if pitch_ignore then
-                self.phase[i] = angle
-            end]]
+            -- estimate instantaneous frequency
+            self.phase_delta[i] = wrap(angle - self.phase[i])
+            self.phase[i] = angle
         end
     else
         for i=0,cplxSize-1 do
-            self.phase[i] =  self.phase[i] + self.phase_delta[i] 
+            -- when frozen, the phase updates w estimated freq
+            self.phase[i] =  wrap(self.phase[i] + self.phase_delta[i] )
         end
     end
-	-- processing magnitudes
-	
-	--[[if pitch_ignore then
-        for i=0,cplxSize-1 do
-            self.syn_mag[i] = self.mag[i]
-            self.syn_phase[i] = self.phase[i]
-        end
-	else]]
-	    local coef = {0,0,1,0} 
-	    local pind = math.floor(warp) 
-        local pfrac = warp - math.floor(warp)
-        
-        for i = 1,4 do
-            local a = warpPolynomials[pind][i]
-            local b = warpPolynomials[pind+1][i]
-            coef[i] = a + (b - a)*pfrac
-        end
+    
+	-- do warp and pitch shift
+    local coef = {0,0,1,0} 
+    local pind = math.floor(warp) 
+    local pfrac = warp - math.floor(warp)
+    
+    -- get polynomial coefficients    
+    for i = 1,4 do
+        local a = warpPolynomials[pind][i]
+        local b = warpPolynomials[pind+1][i]
+        coef[i] = a + (b - a)*pfrac
+    end
 	    
 	
-        local i2 = 0
-        local increment = 1/pitch
-        for i=0,cplxSize-1 do
-            local x = i / (cplxSize-1)
-            x = coef[4] + x*(coef[3] +x*(coef[2] + x*coef[1]))
-            x = x*(cplxSize-1)
+    local i2 = 0
+    local increment = 1/pitch
+    for i=0,cplxSize-1 do
+        local x = i / (cplxSize-1)
+        -- compute polynimial warping
+        x = coef[4] + x*(coef[3] +x*(coef[2] + x*coef[1]))
+        x = x*(cplxSize-1)
             
-            i2 = x*increment
-            local ind = math.floor(i2)
-            local frac = i2 - math.floor(i2)
+        i2 = x*increment
+        local ind = math.floor(i2)
+        local frac = i2 - math.floor(i2)
 	    
-            if i2<cplxSize and i2>0 then
-                local mag_a = self.mag[ind]
-                local mag_b = self.mag[ind+1]
+        if i2<cplxSize and i2>0 then
+            local mag_a = self.mag[ind]
+            local mag_b = self.mag[ind+1]
                 
-                local ph_a = self.phase[ind]
-                local ph_b = self.phase[ind+1]
+            local ph_a = self.phase[ind]
+            local ph_b = self.phase[ind+1]
                 
-                local del_a = self.phase_delta[ind]
-                local del_b = self.phase_delta[ind+1]
+            local del_a = self.phase_delta[ind]
+            local del_b = self.phase_delta[ind+1]
 	    
 		
-                self.syn_mag[i] = mag_a + (mag_b - mag_a) * frac 
+            self.syn_mag[i] = mag_a + (mag_b - mag_a) * frac 
                 
-                local delta = del_a  + (del_b  - del_a ) * frac
-                
-                self.syn_phase[i] = (ph_a  + (ph_b  - ph_a ) * frac + math.pi + delta*pitch)%(2*math.pi) - math.pi 
-            end
-            
+            -- interpolate phase
+            -- no attempt is made to correct for the pitch shift
+            self.syn_phase[i] = wrap(ph_a  + wrap(ph_b  - ph_a ) * frac)
         end
-    --end
+            
+    end
+    
 
     --update mag
     local max = 0
@@ -195,14 +208,9 @@ local function ApplyFilter(self)
     end
     
     
-    -- update phases
+    -- phase randomisation
     for i=0,cplxSize-1 do
-        
-        if not freeze then
-		    self.phase[i] = self.syn_phase[i] 
-		end
-		
-		self.syn_phase[i] = self.syn_phase[i] + 6*phase_random*math.random()
+		self.syn_phase[i] = self.syn_phase[i] + TWOPI*phase_random*(math.random()-0.5)
 	end
 	
 	-- resynthesis
@@ -212,9 +220,6 @@ local function ApplyFilter(self)
 		local phase = self.syn_phase[i]
 		spectrum[i][0] = mag * math.cos(phase)
 		spectrum[i][1] = mag * math.sin(phase)
-		
-		--self.feedb_buf[i][0] = (1.0-feedback)*spectrum[i][0] + self.feedb_buf[i][0]
-		--self.feedb_buf[i][1] = (1.0-feedback)*spectrum[i][1] + self.feedb_buf[i][1]
 	end
 end
 
@@ -258,14 +263,6 @@ params = plugin.manageParams {
 		changed = function(val) pitch = 2^(val/12) end;
 	};
 	{
-		name = "random phase";
-		min = 0;
-		max = 1;
-		type = "double";
-		default = 0;
-		changed = function(val) phase_random = val*val end;
-	};
-	{
 		name = "warp";
 		min = 1;
 		max = 4;
@@ -273,6 +270,15 @@ params = plugin.manageParams {
 		default = 0;
 		changed = function(val) warp = val end;
 	};
+	{
+		name = "random phase";
+		min = 0;
+		max = 1;
+		type = "double";
+		default = 0;
+		changed = function(val) phase_random = val end;
+	};
+
 	{
 		name = "feedback";
 		min = 0;
@@ -319,5 +325,12 @@ params = plugin.manageParams {
 		values = {"off"; "on"};
 		default = "off";
 		changed = function(val) freeze = (val == "on") end;
+	};
+		{
+		name = "random feedback";
+		type = "list";
+		values = {"off"; "on"};
+		default = "off";
+		changed = function(val) randomstore = (val == "on") end;
 	};
 }
